98d7a7ae997856ced469903e303f036645c91940
AskAshut0sh <92803090+AskAshut0sh@users.noreply.github.com>
2021-10-20 23:47:26 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+d1={"burgeon":"flourish","manticore":"scorpion","mojo":"success","colubrine":"snake like"}
+print("Enter your word")
+n1=input()
+#print("Your meaning is",d1.get(n1))
+print("Your meaning is")
+print(d1[n1])
